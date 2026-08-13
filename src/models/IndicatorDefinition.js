451a3aea65c8ab5618b6e8d0d4a1300f8f27d36f
BalvarezSoto <balvarez@usm.cl>
2026-08-12 22:30:55 -0400

    fix(BBDD):Corrección-de-importación-de-tabla-de-meta (#28)
    Se corrigio la importación de la tabla de metas

--- src/models/IndicatorDefinition.js
+++ src/models/IndicatorDefinition.js
@@ -15,6 +15,7 @@ const IndicatorDefinition = sequelize.define('IndicatorDefinition', {
   key: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
     validate: { notEmpty: true }
   },
   name: {
